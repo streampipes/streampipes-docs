0c45fa3817136227cf58f5ca2b64db38fa413ce5
Dominik Riemer <riemer@fzi.de>
2018-11-10 13:44:30 +0100

    Extract scripts only required for production

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -3,16 +3,3 @@ $(document).ready(function () {
         dots: true
     });
 });
-
-var _paq = _paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-    var u="//kalmar46.fzi.de/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
