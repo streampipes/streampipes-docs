528cc4a14e23a6626f894f9c0e3a3e820e56ea22
Dominik Riemer <riemer@fzi.de>
2019-08-19 00:19:12 +0200

    streampipes/ce#236: Improve visibility of carousel

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     $('.my-carousel').slick({
-        dots: true
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
     $('.title-carousel').slick({
         dots: true,
